ffcc8f7f058d4a0e48d4efed9ded9c00dbb2ef8c
岩永千輝 <52753117+Senki-dayo@users.noreply.github.com>
2023-09-20 09:12:25 +0000

    update: exif情報を取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
 import "./styles.css";
+import React, { useCallback, useRef } from "react";
 import EXIF from "exif-js";
 
 export default function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onChange = useCallback(() => {
+    if (inputRef.current?.files && inputRef.current.files.length > 0) {
+      const reader = new FileReader();
+      reader.addEventListener(
+        "load",
+        () => {
+          getExif(reader.result as string);
+        },
+        false,
+      );
+      reader.readAsDataURL(inputRef.current.files[0]);
+    }
+  }, []);
+
+  function getExif(url: string) {
+    const image = new Image();
+    image.src = url;
+    image.onload = function () {
+      EXIF.getData(image as any, function () {
+        //EXIF情報の緯度・経度タグを取得
+        const gpsLatitude = EXIF.getTag(image, "GPSLatitude");
+        const gpgLongitude = EXIF.getTag(image, "GPSLongitude");
+
+        //度分秒を10進数に変換
+        const lat =
+          gpsLatitude[0] / 1 + gpsLatitude[1] / 60 + gpsLatitude[2] / 3600;
+        const lon =
+          gpgLongitude[0] / 1 + gpgLongitude[1] / 60 + gpgLongitude[2] / 3600;
+        console.log(`緯度=${lat}`);
+        console.log(`経度=${lon}`);
+      });
+    };
+  }
+
   return (
     <div className="App">
       <h1>exif-js Test</h1>
+      <input ref={inputRef} onChange={onChange} type="file" />
     </div>
   );
 }
