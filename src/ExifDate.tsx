bf590409f34a23ad358c8ff89965185536bef0d3
岩永千輝 <52753117+Senki-dayo@users.noreply.github.com>
2023-09-20 10:11:08 +0000

    update: ファイル構成変更

---
+++ src/ExifDate.tsx
@@ -0,0 +1,45 @@
+import "./styles.css";
+import React, { useCallback, useRef } from "react";
+import EXIF from "exif-js";
+
+export default function ExifDate() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onChange = useCallback(() => {
+    if (inputRef.current?.files && inputRef.current.files.length > 0) {
+      const reader = new FileReader();
+      reader.addEventListener(
+        "load",
+        () => {
+          getExif(reader.result as string);
+        },
+        false,
+      );
+      reader.readAsDataURL(inputRef.current.files[0]);
+    }
+  }, []);
+
+  function getExif(url: string) {
+    const image = new Image();
+    image.src = url;
+    image.onload = function () {
+      EXIF.getData(image as any, function () {
+        // 保有している情報を確認
+        console.log(EXIF.pretty(image));
+
+        //日時の情報を取得
+        const original_date = EXIF.getTag(image, "DateTimeOriginal");
+        const updated_date = EXIF.getTag(image, "DateTime");
+        console.log(original_date);
+        console.log(updated_date);
+      });
+    };
+  }
+
+  return (
+    <div className="App">
+      <h1>exif-js Date Test</h1>
+      <input ref={inputRef} onChange={onChange} type="file" />
+    </div>
+  );
+}
--- src/ExifLocate.tsx
+++ src/ExifLocate.tsx
@@ -2,7 +2,7 @@ import "./styles.css";
 import React, { useCallback, useRef } from "react";
 import EXIF from "exif-js";
 
-export default function App() {
+export default function ExifLocate() {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const onChange = useCallback(() => {
@@ -27,18 +27,22 @@ export default function App() {
         // 保有している情報を確認
         console.log(EXIF.pretty(image));
 
-        //日時の情報を取得
-        const original_date = EXIF.getTag(image, "DateTimeOriginal");
-        const updated_date = EXIF.getTag(image, "DateTime");
-        console.log(original_date);
-        console.log(updated_date);
+        //EXIF情報の緯度・経度タグを取得
+        const gpsLatitude = EXIF.getTag(image, 'GPSLatitude')
+        const gpgLongitude = EXIF.getTag(image, 'GPSLongitude')
+
+        //度分秒を10進数に変換
+        const lat = gpsLatitude[0]/1 + gpsLatitude[1]/60 + gpsLatitude[2]/3600
+        const lon = gpgLongitude[0]/1 + gpgLongitude[1]/60 + gpgLongitude[2]/3600
+        console.log(`緯度=${lat}`)
+        console.log(`経度=${lon}`)
       });
     };
   }
 
   return (
     <div className="App">
-      <h1>exif-js Test</h1>
+      <h1>exif-js Locate Test</h1>
       <input ref={inputRef} onChange={onChange} type="file" />
     </div>
   );
