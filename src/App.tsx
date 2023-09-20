0b8707cfe04a24504d2ea586bf5f95a445db8fd3
岩永千輝 <52753117+Senki-dayo@users.noreply.github.com>
2023-09-20 08:52:20 +0000

    update: install exif-js

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./styles.css";
+import EXIF from "exif-js";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>exif-js Test</h1>
     </div>
   );
 }
